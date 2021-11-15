dd29a928da1f1592b25085207a5a305f619a3d6b
Richz97 <94222721+Richz97@users.noreply.github.com>
2021-11-15 19:24:48 +0100

    Update motion_controller.cpp

--- src/motion_controller.cpp
+++ src/motion_controller.cpp
@@ -2,20 +2,19 @@
 #include "experimental_assignment1/Motion.h"
 #include "std_msgs/Bool.h"
 
-bool sim_move(experimental_assignment1::Motion::Request &req, experimental_assignment1::Motion::Response &res);
+void simulate_motion(experimental_assignment1::Motion::Request &req, experimental_assignment1::Motion::Response &res);
 
 int main(int argc, char **argv){
     ros::init(argc, argv, "motion_controller");
     ros::NodeHandle n;
-    ros::ServiceServer srv_1=n.advertiseService("/change_room", sim_move);
+    ros::ServiceServer srv_1=n.advertiseService("/change_room", simulate_motion);
     while(ros::ok()){
         ros::spinOnce();
     }
     return 0;
 }
 
-bool sim_move(experimental_assignment1::Motion::Request &req, experimental_assignment1::Motion::Response &res){
+void simulate_motion(experimental_assignment1::Motion::Request &req, experimental_assignment1::Motion::Response &res){
     sleep(1);
     res.ok=true;
-    return 1;
 }
